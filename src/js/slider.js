235df2cb4b12859dffbd6f60a43dc5f8a7d490ca
letmewin22 <lkosteckiy5@gmail.com>
2021-12-18 13:00:08 +0200

    finish slider

--- src/js/slider.js
+++ src/js/slider.js
@@ -1,16 +1,64 @@
+import swipeDetect from './swipe.js'
+
 export const slider = () => {
+  let currentSlide = 0
   const $slider = document.querySelector('[data-slider]')
   const $slides = $slider.querySelectorAll('[data-slide]')
-
-  console.log($slides)
 
   const $navPrev = $slider.querySelector('[data-prev]')
   const $navNext = $slider.querySelector('[data-next]')
 
-  const prevHandler = () => {}
+  const setCurrentSlide = () => {
+    $slides.forEach(slide => {
+      slide.classList.remove('slide--active')
+    })
 
-  const nextHandler = () => {}
+    $slides[currentSlide].classList.add('slide--active')
+  }
+
+  setCurrentSlide()
+
+  const prevHandler = () => {
+    currentSlide--
+
+    if (currentSlide < 0) {
+      currentSlide = $slides.length - 1
+    }
+
+    setCurrentSlide()
+  }
+
+  const nextHandler = () => {
+    currentSlide++
+
+    if (currentSlide > $slides.length - 1) {
+      currentSlide = 0
+    }
+
+    setCurrentSlide()
+  }
 
   $navPrev.addEventListener('click', prevHandler)
   $navNext.addEventListener('click', nextHandler)
+
+  swipeDetect($slider, dir => {
+    if (dir === 'left') {
+      nextHandler()
+    }
+
+    if (dir === 'right') {
+      prevHandler()
+    }
+  })
+
+  const onKey = e => {
+    if (e.key === 'ArrowLeft') {
+      prevHandler()
+    }
+    if (e.key === 'ArrowRight') {
+      nextHandler()
+    }
+  }
+
+  window.addEventListener('keydown', onKey)
 }
